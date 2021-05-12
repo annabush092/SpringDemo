cd948fabe598e2aa70aecc2e6f8089b86e99ebe8
Anna Bush <annabush092@gmail.com>
2021-05-12 10:44:28 -0400

    first commit

---
+++ src/main/java/com/annabush/demo/VendingMachine.java
@@ -0,0 +1,37 @@
+package com.annabush.demo;
+
+import java.util.*;
+
+public class VendingMachine {
+    boolean on;
+    ArrayList<String> items;
+
+    public VendingMachine() {
+        items = new ArrayList<String>();
+    }
+
+    public void powerButton() {
+        on = !on;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public int numItems() {
+        return items.size();
+    }
+
+    public void addItems(ArrayList<String> itemsToAdd) {
+        items.addAll(itemsToAdd);
+    }
+
+    public void removeItemAt(int i) {
+        items.remove(i);
+    }
+
+    public boolean itemsInclude(String item) {
+        return items.contains(item);
+    }
+
+}
---
+++ src/test/java/com/annabush/demo/VendingMachineTest.java
@@ -0,0 +1,63 @@
+package com.annabush.demo;
+
+
+import java.io.*;
+import java.util.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VendingMachineTest {
+
+    VendingMachine vm;
+    @BeforeEach
+    public void setUp() {
+        vm = new VendingMachine();
+    }
+
+    @Test
+    public void vmShouldTurnOnOffWhenSwitchedOnOff() {
+        assertFalse(vm.isOn());
+
+        vm.powerButton();
+
+        assertTrue(vm.isOn());
+
+        vm.powerButton();
+
+        assertFalse(vm.isOn());
+    }
+
+    @Test
+    public void vmCanAddItems() {
+        add3Items();
+
+        assertTrue(vm.numItems() == 3);
+
+        ArrayList<String> secondAddition = new ArrayList<String>();
+        secondAddition.add("chocolate");
+
+        vm.addItems(secondAddition);
+
+        assertTrue(vm.numItems() == 4);
+    }
+
+    @Test
+    public void vmCanRemoveItems() throws IOException {
+
+        add3Items();
+
+        vm.removeItemAt(1);
+
+        assertTrue(vm.numItems() == 2);
+        assertFalse(vm.itemsInclude("granola bar"));
+    }
+
+    private void add3Items() {
+        ArrayList<String> newItems = new ArrayList<String>();
+        newItems.add("candy");
+        newItems.add("granola bar");
+        newItems.add("peanuts");
+
+        vm.addItems(newItems);
+    }
+}
